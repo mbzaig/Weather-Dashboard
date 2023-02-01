caea3fd53b2f08aaa06d28a34100058f2490afdb
MBZAIG <muzzab@amazon.co.uk>
2023-02-01 16:07:58 +0000

    Added fetch query for the city and weather forecast

--- script.js
+++ script.js
@@ -7,7 +7,58 @@ $(document).ready(function () {
 
 })
 
-var APIKey="16b5177dc1a814042da65ece74800fb0";
-var city="London";
-var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
-console.log(queryURL);
+var APIKey = "16b5177dc1a814042da65ece74800fb0";
+// var city="Manchester";
+ 
+// console.log("Queries 5 Day data" + queryURL5Day);
+
+var searchBtn = document.getElementById("search-button");
+var searchCity = document.getElementById("search-input");
+
+
+function citySearch(search) {
+
+    var queryURLCity = "http://api.openweathermap.org/geo/1.0/direct?q=" + search + "&limit=5&appid=" + APIKey;
+    console.log("Queries the City " + queryURLCity);
+
+
+    fetch(queryURLCity)
+        .then((response) => response.json())
+        .then((data) => {
+            console.log(data[0]);
+            weatherSearch(data[0]);
+        });
+
+}
+
+
+
+function weatherSearch(location) {
+    var { lat, lon } = location;
+    var city = location.name;
+
+var queryURL5Day = "http://api.openweathermap.org/data/2.5/forecast?lat="+lat+"&lon="+lon+"&appid="+APIKey;
+fetch(queryURL5Day)
+.then((response) => response.json())
+.then((data) => {
+    console.log(data);
+    displayItems(city, data);
+});
+
+}
+
+searchBtn.addEventListener("click", validCity);
+function validCity(e) {
+    if (!searchCity.value) {
+        return;
+    }
+    e.preventDefault();
+    var search = searchCity.value.trim();
+    citySearch(search);
+}
+
+function displayItems(city, data){
+ current(city, data.list[0]);
+ forecast(data.list);
+    
+}
