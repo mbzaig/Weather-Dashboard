cd80d566bec553b2a2d54713d686c347877194f1
MBZAIG <muzzab@amazon.co.uk>
2023-03-08 21:01:32 +0000

    Added forecast info display to html

--- assets/script.js
+++ assets/script.js
@@ -37,11 +37,11 @@ function weatherSearch(location) {
     var { lat, lon } = location;
     var city = location.name;
 
-    var queryURL5Day = "http://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=" + APIKey +"&units=metric";
+    var queryURL5Day = "http://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=" + APIKey + "&units=metric";
     fetch(queryURL5Day)
         .then((response) => response.json())
         .then((data) => {
-           console.log(data);
+            console.log(data);
             displayItems(city, data);
         });
 
@@ -58,9 +58,9 @@ function validCity(e) {
 }
 
 function displayItems(city, data) {
-    
+
     current(city, data.list[0]);
-    
+
     forecast(city, data.list);
 
 }
@@ -68,23 +68,56 @@ function displayItems(city, data) {
 function current(place, info) {
     // Extract the relevant data from the API response
     var city = place;
-    console.log("This City "+city);
-    console.log ("info==="+info.main);
+    console.log("This City " + city);
+    
     var date = moment.unix(info.dt).format("dddd, MMMM Do");
     var icon = $("<img>").attr(
-      "src",
-      "https://openweathermap.org/img/w/" + info.weather[0].icon + ".png"
+        "src",
+        "https://openweathermap.org/img/w/" + info.weather[0].icon + ".png"
     );
     var temp = $("<p>").text("Temperature: " + info.main.temp + " °C");
     var humidity = $("<p>").text("Humidity: " + info.main.humidity + "%");
     var windSpeed = $("<p>").text("Wind Speed: " + info.wind.speed + " m/s");
-  
+
     // Clear the current weather div and add the new content
     $("#current-weather").empty();
     $("#current-weather")
-      .append($("<h2>").text(city + " (" + date + ")"))
-      .append(icon)
-      .append(temp)
-      .append(humidity)
-      .append(windSpeed);
-  }
+        .append($("<h3>").text(city + " (" + date + ")"))
+        .append(icon)
+        .append(temp)
+        .append(humidity)
+        .append(windSpeed);
+}
+
+
+function forecast(place, info) {
+    // Clear the forecast div and add a heading
+    $("#forecast").empty().append($("<h2>").text("5-Day Forecast:"));
+    var city=place;
+    // Loop through the forecast data and display each day's weather
+    for (var i = 0; i < info.length; i++) {
+        // Only display the weather for 12:00 PM each day
+        if (info[i].dt_txt.indexOf("12:00:00") !== -1) {
+            // Extract the relevant data from the API response
+            var date = moment.unix(info[i].dt).format("ddd, MMMM Do");
+            var icon = $("<img>").attr(
+                "src",
+                "https://openweathermap.org/img/w/" +
+                info[i].weather[0].icon +
+                ".png" 
+            );
+            var temp = $("<p>").text("Temp: " + info[i].main.temp + " °C");
+            var humidity = $("<p>").text(
+                "Humidity: " + info[i].main.humidity + "%"
+            );
+            $("#city-forecast")
+            .append($("<h2>").text(city ))
+            // Create a div for each day's weather and append it to the forecast div
+            var card = $("<div>").addClass("card bg-primary text-white");
+            var cardBody = $("<div>").addClass("card-body p-2");
+            cardBody.append($("<h5>").text(city), date, icon, temp, humidity); // append card body to the card
+            card.append(cardBody); // append card body to the card
+            $("#forecast").append(card);
+        }
+    }
+}
