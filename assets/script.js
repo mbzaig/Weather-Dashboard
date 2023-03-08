28eefd96567b2567ff20a6f7641d30550f78d026
MBZAIG <muzzab@amazon.co.uk>
2023-03-08 19:37:27 +0000

    Added today's weather info

--- assets/script.js
+++ assets/script.js
@@ -9,7 +9,7 @@ $(document).ready(function () {
 
 var APIKey = weatherApi;
 // var city="Manchester";
- 
+
 // console.log("Queries 5 Day data" + queryURL5Day);
 
 var searchBtn = document.getElementById("search-button");
@@ -37,13 +37,13 @@ function weatherSearch(location) {
     var { lat, lon } = location;
     var city = location.name;
 
-var queryURL5Day = "http://api.openweathermap.org/data/2.5/forecast?lat="+lat+"&lon="+lon+"&appid="+APIKey;
-fetch(queryURL5Day)
-.then((response) => response.json())
-.then((data) => {
-    console.log(data);
-    displayItems(city, data);
-});
+    var queryURL5Day = "http://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=" + APIKey +"&units=metric";
+    fetch(queryURL5Day)
+        .then((response) => response.json())
+        .then((data) => {
+           console.log(data);
+            displayItems(city, data);
+        });
 
 }
 
@@ -57,8 +57,34 @@ function validCity(e) {
     citySearch(search);
 }
 
-function displayItems(city, data){
- current(city, data.list[0]);
- forecast(city, data.list);
+function displayItems(city, data) {
     
+    current(city, data.list[0]);
+    
+    forecast(city, data.list);
+
 }
+
+function current(place, info) {
+    // Extract the relevant data from the API response
+    var city = place;
+    console.log("This City "+city);
+    console.log ("info==="+info.main);
+    var date = moment.unix(info.dt).format("dddd, MMMM Do");
+    var icon = $("<img>").attr(
+      "src",
+      "https://openweathermap.org/img/w/" + info.weather[0].icon + ".png"
+    );
+    var temp = $("<p>").text("Temperature: " + info.main.temp + " °C");
+    var humidity = $("<p>").text("Humidity: " + info.main.humidity + "%");
+    var windSpeed = $("<p>").text("Wind Speed: " + info.wind.speed + " m/s");
+  
+    // Clear the current weather div and add the new content
+    $("#current-weather").empty();
+    $("#current-weather")
+      .append($("<h2>").text(city + " (" + date + ")"))
+      .append(icon)
+      .append(temp)
+      .append(humidity)
+      .append(windSpeed);
+  }
